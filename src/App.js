35af23dcca37a33e4200b8a2a36c16b3002572bf
mirvarimuradova <mirvarimuradova7@gmail.com>
2023-12-24 23:21:09 +0400

    tatli

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from './components/Header';
+import Main from "./components/Main";
+import Footer from './components/Footer';
+import Vacancies from './components/Vacancies';
+import About from "./components/About";
+import Contact from "./components/Contacs";
+import { Route,Routes } from 'react-router-dom';
+import List from './components/List';
+import { CartProvider } from 'react-use-cart';
+import Snowfall from 'react-snowfall';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CartProvider>
+      <div className='snowfall' style={{position: 'relative' }}>
+      <Header/>
+      
+      <Routes>
+      <Route  path="/" element={<Main/>} />
+      <Route path='/about' element={<About/>} />
+      
+      <Route path="/vacancies" element={<Vacancies/>} />
+      <Route path="/contacts" element={<Contact/>} />
+      <Route path='/list' element={<List/>}/>
+    
+      
+    </Routes>
+    
+    <Snowfall />
+       </div>
+      
+      </CartProvider>
     </div>
   );
 }
